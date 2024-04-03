9fc2e43fa8951e472d4aff2fe4d55867a0da7caa
Gabriel Pires <gabpiresdev@gmail.com>
2024-04-03 11:18:09 -0300

    feat: new route to get attendee badge data

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { createEvent } from './routes/create-event'
+import { getAttendeeBadge } from './routes/get-attendee-badge'
 import { getEvent } from './routes/get-event'
 import { registerForEvent } from './routes/register-for-event'
 
@@ -15,6 +16,7 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getAttendeeBadge)
 app.register(getEvent)
 
 app
