b9d4acdc5b0989ebb1348f722f650c6c56e42343
Gabriel Pires <gabpiresdev@gmail.com>
2024-04-03 10:03:50 -0300

    feat: integrate fastify type provider zod to handle with schema types

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,17 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
-import z from 'zod'
-import { generateSlug } from './utils/generate-slug'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+
+import { createEvent } from './routes/create-event'
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-  log: ['query'],
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.post('/events', async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-
-  const { details, maximumAttendees, title } = createEventSchema.parse(
-    request.body,
-  )
-
-  const slug = generateSlug(title)
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximumAttendees,
-      slug,
-    },
-  })
-
-  return reply.code(201).send({
-    eventId: event.id,
-  })
-})
+app.register(createEvent)
 
 app
   .listen({
